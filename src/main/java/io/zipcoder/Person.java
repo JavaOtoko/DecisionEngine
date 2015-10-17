e803720cc0e83cc224a2c9d13af952313c98e68a
Pablo Cano <pcano@Zips-MacBook-Pro-98.local>
2015-10-16 23:22:22 -0400

    Adding the Person class for decision engine

---
+++ src/main/java/io/zipcoder/Person.java
@@ -0,0 +1,23 @@
+package io.zipcoder;
+
+/**
+ * Class named Person that will hold customers information for loan decision
+ * Created by pcano on 10/16/15.
+ */
+
+public class Person {
+
+    private String name;
+    private String Address;
+    private String lengthAtResidence;
+    private double rent;
+    private int yearsEmployed;
+    private double salary;
+    private double monthlyIncome;
+    private String maritalStatus;
+    private double disposableIncome;
+    private int FICO;
+    private String collateral;
+    private String capital;
+    
+}
